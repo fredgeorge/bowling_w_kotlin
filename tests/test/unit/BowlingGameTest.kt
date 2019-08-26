59cbf0a259d152b9c0fdba63b76497424a5189de
Fred George <fredgeorge@acm.org>
2019-08-27 10:12:35 +1200

    Fred: Bowling with strikes

--- tests/test/unit/BowlingGameTest.kt
+++ tests/test/unit/BowlingGameTest.kt
@@ -38,6 +38,25 @@ internal class BowlingGameTest {
         assertEquals(90, game.score())
     }
 
+    @Test internal fun `perfect game`() {
+        roll(10.pins * 12)
+        assertEquals(300, game.score())
+    }
+
+    @Test internal fun `strike on last frame`() {
+        roll(0.pins * 18)
+        roll(10.pins)
+        roll(4.pins * 2)
+        assertEquals(18, game.score())
+    }
+
+    @Test internal fun `consecutive strikes mid-game`() {
+        roll(0.pins * 10)
+        roll(10.pins * 2)
+        roll(4.pins * 6)
+        assertEquals(24 + 18 + 4*6, game.score())
+    }
+
     private fun roll(bowlResult: BowlResult) = roll(listOf(bowlResult))
 
     private fun roll(bowlResults: List<BowlResult>) = game.roll(bowlResults)
